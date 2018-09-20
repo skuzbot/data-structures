8c6f5cd6cc3b3bbffc4310e7fc6a477cba6eb1be
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-20 14:25:50 -0500

    Fix sprint 2: set, sprint 1: pseudo stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.height = 0;
 };
 
 
+Stack.prototype.push = function(value) {
+  this.height++;
+  this.storage[this.height] = value;
+};
+
+Stack.prototype.pop = function() {
+  if (this.height <= 0) {
+    return undefined;
+  } else {
+    var temp = this.storage[this.height];
+    delete this.storage[this.height];
+    this.height--;
+    return temp;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.height;
+};
