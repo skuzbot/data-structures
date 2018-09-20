617173b5016669f803a06fad2e58810451114838
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-19 21:43:02 -0500

    Functional Queue Complete

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,33 @@
-var Queue = function() {
+var Queue = function () {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var lowestKey = 0;
+  var highestKey = -1;
+
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
+  someInstance.enqueue = function (value) {
+    highestKey++;
+    storage[highestKey] = value;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function () {
+    if (highestKey < lowestKey) {
+      return undefined;
+    } else {
+      var temp = storage[lowestKey];
+      delete storage[lowestKey];
+      lowestKey++;
+      return temp;
+    }
+
   };
 
-  someInstance.size = function() {
+  someInstance.size = function () {
+    return highestKey - lowestKey + 1;
   };
 
   return someInstance;
