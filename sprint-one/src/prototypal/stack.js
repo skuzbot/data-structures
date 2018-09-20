a6c6a6bc6ab2aa47f58d11d9c47495ce01a50432
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-19 22:55:20 -0500

    Fix Prototypal Stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Stack = function () {
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.height = 0;
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  this.height++;
+  this.storage[this.height] = value;
+};
 
+stackMethods.pop = function () {
+  if (this.height <= 0) {
+    return undefined;
+  } else {
+    var temp = this.storage[this.height];
+    delete this.storage[this.height];
+    this.height--;
+    return temp;
+  }
+};
+
+stackMethods.size = function () {
+  return this.height;
+};
