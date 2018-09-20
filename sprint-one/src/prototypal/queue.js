c7f0de51616de6e4fe51af90ec9b8056efb873ba
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-19 23:10:59 -0500

    Fix Prototypal Queue Test

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,27 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function () {
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  newQueue.lowestKey = 0;
+  newQueue.highestKey = -1;
+  return newQueue;
 };
 
 var queueMethods = {};
 
-
+queueMethods.enqueue = function(value) {
+  this.highestKey++;
+  this.storage[this.highestKey] = value;
+};
+queueMethods.dequeue = function() {
+  if (this.highestKey < this.lowestKey) {
+    return undefined;
+  } else {
+    var temp = this.storage[this.lowestKey];
+    delete this.storage[this.lowestKey];
+    this.lowestKey++;
+    return temp;
+  }
+};
+queueMethods.size = function() {
+  return this.highestKey - this.lowestKey + 1;
+};
