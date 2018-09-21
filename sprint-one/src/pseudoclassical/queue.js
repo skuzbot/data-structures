0d97259a533afca6b41e409b2e98990bfc2cf655
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-21 08:55:37 -0500

    Finish pseudo queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,26 @@
 var Queue = function() {
   this.storage = {};
-  
+  this.smallestKey = 0;
+  this.largestKey = -1;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.largestKey++;
+  this.storage[this.largestKey] = value;
+};
 
+Queue.prototype.dequeue = function () {
+  if (this.largestKey < this.smallestKey) {
+    return undefined;
+  } else {
+    var temp = this.storage[this.smallestKey];
+    delete this.storage[this.smallestKey];
+    this.smallestKey++;
+    return temp;
+
+  }
+};
+
+Queue.prototype.size = function () {
+  return this.largestKey - this.smallestKey + 1;
+};
