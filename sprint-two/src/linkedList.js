dfd76686328517a564ec76a1c95070c90c8b4860
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-21 22:43:59 -0500

    Complete linkedList tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,26 +1,33 @@
 var LinkedList = function() {
   var list = {};
   list.head = null;
-  list.tail = null;
+  list.tail = {};
 
   list.addToTail = function(value) {
-    
-    var tempTail = list.tail;
-    list.tail = Node(value);
-    list.tail.next = tempTail;
-
+    currentNode = Node(value);
     if (list.head === null) {
-      list.head = Node(value);
-      list.head.next = list.tail;
+      list.head = currentNode;
+      list.tail = currentNode;
+    } else {
+      list.tail.next = currentNode;
+      list.tail = currentNode;
     }
-    
   };
 
   list.removeHead = function() {
+    tempHead = list.head;
     list.head = list.head.next;
+    return tempHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    for (currentNode; currentNode !== null; currentNode = currentNode.next) {
+      if (currentNode.value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -39,4 +46,3 @@ var Node = function(value) {
  * Complexity: What is the time complexity of the above functions?
  */
 
- 
