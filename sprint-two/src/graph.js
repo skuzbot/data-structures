708386901ceda12f27072d07a7480697368a5032
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-22 01:15:17 -0500

    End of night progress on graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,49 @@
-
-
-// Instantiate a new graph
-var Graph = function() {
+// Instantiation pseudoclassical
+var Graph = function () {
+  this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(node) {
+Graph.prototype.addNode = function (node) {
+  this.storage[node] = {
+    value: node,
+    connections: {},
+  };
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function (node) {
+  return !!this.storage[node];
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function(node) {
+Graph.prototype.removeNode = function (node) {
+  for (var k in this.storage) {
+    if (this.storage[k].value === node) {
+      delete this.storage[k];
+    }
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function(fromNode, toNode) {
+Graph.prototype.hasEdge = function (fromNode, toNode) {
+  if (this.storage[fromNode].connections.contains(toNode)) {
+    return true;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function(fromNode, toNode) {
+Graph.prototype.addEdge = function (fromNode, toNode) {
+  this.storage[fromNode].connections[toNode] = toNode;
+  this.storage[toNode].connections[fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function(fromNode, toNode) {
-};
+Graph.prototype.removeEdge = function (fromNode, toNode) {};
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function(cb) {
-};
+Graph.prototype.forEachNode = function (cb) {};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
