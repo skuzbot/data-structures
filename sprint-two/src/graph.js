5e40ba62232d417ff80059784dffd521e433d5a6
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-23 10:17:19 -0500

    Complete Graph Tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,10 +1,10 @@
 // Instantiation pseudoclassical
-var Graph = function () {
+var Graph = function() {
   this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function (node) {
+Graph.prototype.addNode = function(node) {
   this.storage[node] = {
     value: node,
     connections: {},
@@ -12,13 +12,16 @@ Graph.prototype.addNode = function (node) {
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function (node) {
+Graph.prototype.contains = function(node) {
   return !!this.storage[node];
 };
 
 // Removes a node from the graph.
-Graph.prototype.removeNode = function (node) {
+Graph.prototype.removeNode = function(node) {
   for (var k in this.storage) {
+    if (this.storage[k].connections[node]) {
+      delete this.storage[k].connections[node];
+    }
     if (this.storage[k].value === node) {
       delete this.storage[k];
     }
@@ -26,23 +29,33 @@ Graph.prototype.removeNode = function (node) {
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
-Graph.prototype.hasEdge = function (fromNode, toNode) {
-  if (this.storage[fromNode].connections.contains(toNode)) {
+Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this.storage[fromNode].connections.hasOwnProperty(toNode)) {
     return true;
   }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
-Graph.prototype.addEdge = function (fromNode, toNode) {
+Graph.prototype.addEdge = function(fromNode, toNode) {
   this.storage[fromNode].connections[toNode] = toNode;
   this.storage[toNode].connections[fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
-Graph.prototype.removeEdge = function (fromNode, toNode) {};
+Graph.prototype.removeEdge = function(fromNode, toNode) {
+  if (this.storage[fromNode].connections.hasOwnProperty(toNode)) {
+    delete this.storage[fromNode].connections[toNode];
+    delete this.storage[toNode].connections[fromNode];
+  }
+};
 
 // Pass in a callback which will be executed on each node of the graph.
-Graph.prototype.forEachNode = function (cb) {};
+Graph.prototype.forEachNode = function(cb) {
+  for (var k in this.storage) {
+    cb(this.storage[k].value);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
