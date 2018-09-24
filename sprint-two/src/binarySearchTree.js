6604f57a41cd9d6e42d1e6b8609d2771afb5ed0b
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-23 22:43:47 -0500

    Complete bST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,23 +1,52 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
   var newBST = Object.create(bSTMethods);
   newBST.value = value;
-  newBST.right = null;
-  newBST.left = null;
+  newBST.right = undefined;
+  newBST.left = undefined;
   return newBST;
 };
 
 var bSTMethods = {};
 
 bSTMethods.insert = function(value) {
-  (value < this.value) ? this.left = value : this.right = value;
+  if (value === this.value) {
+    return;
+  }
+  if (value < this.value) {
+    if (this.left === undefined) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === undefined) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
 };
 
-bSTMethods.contains = function() {
-
+bSTMethods.contains = function(value) {
+  var currentNode = this;
+  if (currentNode.value === value) {
+    return true;
+  } else if (currentNode.left === undefined && currentNode.right === undefined) {
+    return false;
+  } else {
+    return (value < currentNode.value) ? currentNode.left.contains(value) : currentNode.right.contains(value);
+  }
 };
 
-bSTMethods.depthFirstLog = function() {
-
+bSTMethods.depthFirstLog = function(cb) {
+  var currentNode = this;
+  cb(this.value);
+  if (this.left !== undefined) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right !== undefined) {
+    this.right.depthFirstLog(cb);
+  }
 };
 
 
